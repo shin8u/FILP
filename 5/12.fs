d6d5b94788e23ef2b92ef8c31fc8c45acbed9df9
shin8u <98867521+shin8u@users.noreply.github.com>
2022-04-05 14:05:19 +0300

    Update 12.fs

--- 5/12.fs
+++ 5/12.fs
@@ -1,1 +1,18 @@
+open System
+
+[<EntryPoint>]
+let main argv =
+   let otvet (otvet:string) =
+        match otvet.Trim() with
+          |"F#"|"Prolog" -> printfn "Подлиза"
+          |"python"-> printfn "попался, питонист!"
+          |other -> printfn "помойся трижды с мылом"
+    
+   printfn "на чём прогаешь?"
+   //суперпозиция
+   (Console.ReadLine>>otvet>>Console.WriteLine)()
+   //каррирование
+   let f otvet func out = out(func(otvet()))
+   f Console.ReadLine otvet Console.WriteLine 
+   0 
 
